5a994f89271dc36004f67cd86fbf5396d83456a0
Emile Nijssen <emile@athom.nl>
2017-10-08 12:58:50 +0200

    rewrite to sdkv2

--- app.js
+++ app.js
@@ -1,9 +1,154 @@
 "use strict";
 
-var https		= require('https');
-var querystring = require('querystring');
+const Homey = require('homey');
+const Slack = require('./lib/Slack.js');
 
-var Slack 		= require('./lib/Slack.js');
+const POLL_INTERVAL = 1000 * 60 * 60; // 1h
+
+class SlackApp extends Homey.App {
+	
+	onInit() {
+		
+		this._users = [];
+		this._channels = [];
+			
+		this._slack = new Slack();
+		
+		this._initAuth();
+		this._initFlow();
+		
+		this._refresh();
+		this._refreshInterval = setInterval(this._refresh.bind(this), POLL_INTERVAL);
+	}
+	
+	_refresh() {
+		
+		if( !this._slack.isAuthorized() ) return;
+		
+		let channels = this._slack.getChannels()
+			.then( channels => this._channels = channels );
+			
+		let users = this._slack.getUsers()
+			.then( users => this._users = users );
+			
+		return Promise.all([ channels, users ])
+			.catch( this.error );
+	}
+	
+	_initAuth() {
+		let auth = this._getAuth();
+		if( typeof auth === 'undefined' ) {
+			this._setAuth();
+		} else {
+			this._slack.setAccessToken( auth.access_token );			
+		}		
+	}
+	
+	_getAuth() {
+		return Homey.ManagerSettings.get('auth');
+	}
+	
+	_setAuth( access_token, team_name, team_id ) {
+		Homey.ManagerSettings.set('auth', {
+		    access_token: access_token,
+		    team_name: team_name,
+		    team_id: team_id
+		});
+		
+		this._slack.setAccessToken( access_token );
+		
+		this._users = [];
+		this._channels = [];
+		
+		this._refresh();
+		
+	}
+	
+	authorize() {
+		return new Promise((resolve, reject) => {
+			
+			let url = Slack.getOAuth2Url();
+			new Homey.CloudOAuth2Callback(url)
+				.on('url', resolve)
+				.on('code', code => {
+					this._slack.getToken(code)
+						.then( result => {
+							this._setAuth(result.access_token, result.team_name, result.team_id);
+							Homey.ManagerApi.realtime('authorized', true);
+						})
+						.catch(err => {
+							this.error(err);
+							Homey.ManagerApi.realtime('authorized', false);
+						})
+				})
+				.generate()
+				.catch(err => {
+					this.error(err);
+					reject(err);
+				})
+				
+			
+		});
+	}
+	
+	/*
+		Flow
+	*/
+	_initFlow() {
+		
+		new Homey.FlowCardAction('send_message')
+			.register()
+			.registerRunListener( args => {
+		
+				if( !this._slack.isAuthorized() )
+					throw new Error( Homey.__('unauthorized') );
+				
+				return this._slack.postMessage({
+					text		: args.message,
+					channel		: args.recipient.id,
+					username	: 'Homey',
+					icon_url	: 'https://homey-static.athom.com/apps/com.slack/icon.png'					
+				});
+			})
+			.getArgument('recipient')
+			.registerAutocompleteListener(this._onFlowRecipientAutocomplete.bind(this));
+		
+	}
+	
+	_onFlowRecipientAutocomplete( query ) {
+		
+		if( !this._slack.isAuthorized() )
+			return Promise.reject( new Error( Homey.__('unauthorized') ) );
+			
+		let result = [];
+		
+		this._channels.forEach(channel => {
+			if( channel.is_archived ) return;
+			result.push({
+				id: channel.id,
+				name: '#' + channel.name
+			});
+		});
+		
+		this._users.forEach(user => {
+			if( user.deleted ) return;
+			result.push({
+				id: user.id,
+				name: user.profile.real_name + ' (@' + user.name + ')',
+				image: user.profile.image_72				
+			});
+		})
+			
+		return result.filter(recipient => {
+			return recipient.name.toLowerCase().indexOf(query.toLowerCase()) > -1;			
+		});
+	}
+	
+}
+
+module.exports = SlackApp;
+
+return;
 
 var slack;
 var recipients = false;
@@ -96,89 +241,5 @@ function initSlack() {
 			}
 		});
 
-	}
-}
-
-/*
-	Authorize with Slack
-*/
-function authorize( callback ) {
-
-	callback = callback || function(){}
-
-	var callback_called = false;
-
-	Homey.manager('cloud').generateOAuth2Callback(
-		'https://slack.com/oauth/authorize?client_id=' + Homey.env.CLIENT_ID + '&scope=chat:write:bot,channels:read,users:read',
-		onGotUrl,
-		onGotCode
-	);
-
-	function onGotUrl( err, url ){
-		if( err ) return callback(err);
-
-		Homey.log('got url:', url);
-		callback( null, url );
-		callback_called = true;
-	}
-
-	function onGotCode( err, code ) {
-		if( err ) {
-		    Homey.manager('api').realtime('authorized', false);
-			return Homey.error(err);
-		}
-
-		Homey.log('Got authorization code!');
-
-		var data = querystring.stringify({
-	        'client_id'		: Homey.env.CLIENT_ID,
-	        'client_secret'	: Homey.env.CLIENT_SECRET,
-	        'code'			: code
-		});
-
-		var options = {
-			host: 'slack.com',
-			path: '/api/oauth.access',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Content-Length': Buffer.byteLength(data)
-			}
-		};
-
-		var req = https.request(options, function(res) {
-			res.setEncoding('utf8');
-			var body = "";
-			res.on('data', function (chunk) {
-				body += chunk;
-			});
-			res.on('end', function(){
-				try {
-					body = JSON.parse(body);
-
-				    if( body.ok !== true ) {
-					    Homey.manager('api').realtime('authorized', false);
-					    return Homey.error( "body not ok" );
-					}
-
-				    Homey.manager('settings').set('auth', {
-					    access_token	: body.access_token,
-					    team_name		: body.team_name,
-					    team_id			: body.team_id
-					});
-
-				    Homey.manager('api').realtime('authorized', true);
-
-					initSlack();
-
-				} catch(e){
-				    Homey.manager('api').realtime('authorized', false);
-					Homey.error(e);
-				}
-			})
-		});
-
-		req.write(data);
-		req.end();
 	}
 }
